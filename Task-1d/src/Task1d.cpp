0bd2d6935ab3c045fba821f4a04a4d5866e58672
mweser <matthew.weser@gmail.com>
2015-11-28 00:32:44 -0700

    Repo cloned and code directory set up

---
+++ Task-1d/src/Task1d.cpp
@@ -0,0 +1,9 @@
+//
+//  Task1d.cpp
+//  cse408_project01
+//
+//  Created by Matthew Weser on 11/28/15.
+//  Copyright © 2015 mweser. All rights reserved.
+//
+
+#include "Task1d.hpp"
---
+++ Task-1d/src/Task1d.hpp
@@ -0,0 +1,39 @@
+//
+//  Task1d.hpp
+//  cse408_project01
+//
+//  Created by Matthew Weser on 11/28/15.
+//  Copyright © 2015 mweser. All rights reserved.
+//
+
+#ifndef Task1d_hpp
+#define Task1d_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+    //OpenCV libraries included
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/imgcodecs.hpp"
+
+using namespace std;
+
+int main()
+{
+   
+    cout << "You are awesome" << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+#endif /* Task1d_hpp */
