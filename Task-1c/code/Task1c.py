631a818938eafd35628a814bb4aa4336dcca0635
rahulkrsna <raahulkrsna@gmail.com>
2015-11-29 20:50:05 -0700

    Task3-1c added

---
+++ Task-1c/code/Task1c.py
@@ -0,0 +1,166 @@
+__author__ = 'rahulkrsna'
+
+import cv2
+from sys import platform as _platform
+import numpy as np
+
+path = ""
+num_components = 0
+frame_height = 64
+frame_width = 64
+videoFilePath = ""
+videoFileName = ""
+outputFilePath = ""
+
+def acceptInput():
+    global path
+    global num_components
+    global videoFilePath
+    global videoFileName
+    global outputFilePath
+
+    #capture frame numbers as input.
+    path = raw_input("Enter the Path of Folder containing video Files: ")
+    if _platform == "linux" or _platform == "linux2":
+        slash = '/'
+    elif _platform == "darwin":
+        slash = '/'
+    elif _platform == "win32":
+        slash = '\\'
+
+    path = path.rstrip(slash)
+    path = path + slash
+
+
+    videoFileName = raw_input('Enter the video file name <v>: ')
+    videoFilePath = path+videoFileName
+    num_components = 9
+    while num_components > 8:
+        num_components = int(raw_input("Enter number of significant wavelet components[must be less than 9]: "))
+        if num_components < 9:
+            break
+
+    outputFilePath = videoFilePath.rsplit(".")[0]
+    outputFilePath = "{0}_framedwt_{1}.bwt".format(outputFilePath,num_components)
+    extractFrames()
+
+# Extracts Frames and Saves the partial frame information
+def extractFrames():
+    global videoFilePath
+    global frame_width
+    global frame_height
+    global num_components
+    global outputFilePath
+    # videoFilePath = '/Users/rahulkrsna/Documents/ASU_Fall2015/MIS/HW-3/CSE598_MIS_Phase_3/Task-1c/R1.mp4'
+
+    cap = cv2.VideoCapture(videoFilePath)
+    frame_width = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
+
+    # Write frame information to the file
+    output_file = open(outputFilePath, 'a') # open the file
+    #Save the Frame Co-ordinates to the file
+    FrameCoor = "Height : {0} , Width : {1} , Wavelets : {2}\n".format(frame_height, frame_width, num_components)
+    output_file.write(FrameCoor)
+
+    frame_id = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if ret == True:
+            frame_id += 1
+            yuvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2YUV)
+            y,u,v = cv2.split(yuvImage)
+            input = y.astype(np.float)
+            # DWT_2d_Transformation(input,frame_id, frame_height, frame_width) # Apply 2d-DWT Transformation
+
+            for i in range(0,frame_height,8):
+                for j in range(0,frame_width,8):
+                    output = DWT_2d_Transformation(input[i:i+8,j:j+8])
+                    saveToFile(frame_id,i,j,output, num_components,output_file)
+            # if frame_id == 1:
+            #     cap.release()
+        else:
+            cap.release()
+
+    output_file.close() #Close the file
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    print("Saved to path {0}".format(outputFilePath))
+
+
+def haar_1D(data):
+  if len(data) == 1:
+    return data.copy()
+
+  sum_avg = (data[0::2] + data[1::2]) / np.sqrt(2.)
+  diff_avg = (data[0::2] - data[1::2]) / np.sqrt(2.)
+
+  return np.hstack((haar_1D(sum_avg), diff_avg))
+
+
+def DWT_2d_Transformation(frame):
+  h,w = frame.shape
+  rows = np.zeros(frame.shape, dtype=float)
+  for y in range(h):
+    rows[y] = haar_1D(frame[y])
+  cols = np.zeros(frame.shape, dtype=float)
+  for x in range(w):
+    cols[:,x] = haar_1D(rows[:,x])
+  return cols
+
+
+# Zig Zag way of saving the data of a matrix.
+def saveToFile(frame_id,coord_x, coord_y,transformed_info, length, output_file):
+
+    rows,cols = transformed_info.shape # # of rows and columns
+    index = 1
+    turn = 0
+    i = j = 0
+    while i < rows and j < cols and index <= length: # Save the m(length) most significant values
+        # print ("{0} -> ({1},{2})".format(transformed_info[i][j],i,j))
+        stringToOutput = "{0},{1},{2},{3},{4} \n".format(frame_id,coord_x,coord_y,index,transformed_info[i][j])
+        # print stringToOutput
+        output_file.write(stringToOutput)
+        index += 1
+
+        if turn == 0 and i == 0 and j != cols-1:
+            j += 1
+            turn = 1
+        elif turn == 1 and j == 0 and i != rows-1:
+            i += 1
+            turn = 0
+        elif turn == 1 and i == rows-1:
+            j += 1
+            turn = 0
+        elif turn ==0 and j == cols-1:
+            i += 1
+            turn = 1
+        elif turn == 0:
+            i -= 1
+            j += 1
+        elif turn == 1:
+            i += 1
+            j -= 1
+
+
+# Unit Test Case
+def testCase():
+    comp = [[10,20,30,40],[20,30,40,50],[30,40,50,60],[40,50,60,70]]
+    arr = np.array(comp)
+    arr = arr.astype(dtype=np.float)
+    DWT_2d_Transformation(arr)
+    print arr
+
+
+# Print to console
+def logPrinter(message):
+    print message
+
+
+def main():
+    acceptInput()
+    # testCase()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Task-1c/code/Task3DWT-1C.py
@@ -0,0 +1,122 @@
+__author__ = 'rahulkrsna'
+
+
+import cv2
+import Task1c as t2
+import numpy as np
+import collections
+
+compare_FrameID = 0
+videoFileName = ""
+selectedFrames = []
+totalMatchFrames = 10
+
+def acceptInput():
+    global videoFileName
+    global compare_FrameID
+
+    videoFileName = raw_input('Enter the .bwt file path: ')
+    compare_FrameID = int(raw_input("Enter FrameID for Comaprison: "))
+
+
+# Compute Eucleadean Distance between every vector and the vector of the chosen frame
+def computeEucleadean(input_file):
+    global compare_FrameID
+    global selectedFrames
+
+    FrameInfo = {}
+    eucledeanInfo = {}
+
+    # Read the input file(.dwt file) and deserailized the data line by line and save the dwt values to a dictionary, based on frames
+    for line in input_file:
+        frameId = int(line.split(',')[0])
+        # blockCoor = line.split(',')[1] +','+line.split(',')[2]
+        # data = np.array([int(line.split(',')[1]),float(line.split(',')[2])])
+        data = float(line.split(',')[4])
+        if frameId in FrameInfo.keys():
+            FrameInfo[frameId].append(data)
+        else :
+            FrameInfo[frameId] = [data]
+
+    # Take the compare frame to be the frameID chosen by the user.
+    keyFrame = FrameInfo[compare_FrameID]
+
+    # Calculate the Eucledean distance for every frame, based on the DWT values
+    # Compute Similarity = 1 / (1+ eucledean_distance)
+    for keys,values in FrameInfo.items():
+        distance = np.linalg.norm(np.array(keyFrame) - np.array(values))
+        similarity = 1. / (1. + distance) # Similarity = 1 / (1+eucledian_distance)
+        if similarity in eucledeanInfo.keys():
+            eucledeanInfo[similarity].append(keys)
+        else:
+            eucledeanInfo[similarity] = [keys]
+
+    # Sort the values in descending order basing on their similarities
+    od_Similarities = collections.OrderedDict(sorted(eucledeanInfo.items(),reverse=True))
+
+    # Print the 10 most similar frames to the chosen frame
+    count = 0
+    print('Similarity :: 1 :: very high similarity')
+    print('Similarity :: 0 :: very low similarity')
+    print('Similarity     - FrameId')
+    for keys,values in od_Similarities.items():
+        for value in values:
+            if compare_FrameID != value:
+                print ("{0} - {1}".format(keys, value))
+                print ""
+                selectedFrames.append(value)
+                count += 1
+
+        if count == 10:
+            break
+
+
+def showFrames():
+    global selectedFrames
+
+    print t2.videoFilePath
+    cap = cv2.VideoCapture(t2.videoFilePath)
+
+    frame_id = 0
+    count = 0
+    while cap.isOpened():
+        ret, frame = cap.read()
+
+        if ret == True:
+            frame_id += 1
+            # If the current frame is the frame chosen display the frame
+            if frame_id in selectedFrames:
+                cv2.imshow(str(frame_id), frame)
+                count += 1
+        else:
+            cap.release()
+
+        # All the 10 frames chosen are displayed so we exit.
+        if count == totalMatchFrames:
+            cap.release()
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def main():
+    global videoFileName
+
+    t2.main() # Call the main function of task-2
+
+    acceptInput() # Accept User Input
+    # Open the input file to read
+    input_file = open(videoFileName, 'r')
+    frameInfo = input_file.readline()
+    rows = int(frameInfo.split(',')[0].split(':')[1].strip(' '))
+    cols = int(frameInfo.split(',')[1].split(':')[1].strip(' '))
+    wavelets = int(frameInfo.split((','))[2].split(':')[1].strip(' '))
+    computeEucleadean(input_file) #Compute Eculedean Distnace and similarities
+    input_file.close() #close the file
+
+    # Output
+    showFrames()
+
+
+# /Users/rahulkrsna/Documents/ASU_Fall2015/MIS/HW-3/CSE598_MIS_Phase_3/Task-2/code/R1_framedwt_10.fwt
+if __name__ == '__main__':
+    main()
